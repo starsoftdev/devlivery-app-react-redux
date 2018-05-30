9c2a166280bce364b9e40407282294428eb3fdbd
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-05-30 03:11:33 +0200

    refactor styles - create initial global/Button/Input styles, stop using ant styles for all components, add fonts

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,20 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Layout.css'
-import {Footer, Header} from '../../components'
+import {Header} from '../../components'
 import {Layout} from 'antd'
-import antdStyles from 'antd/dist/antd.min.css'
-import globalStyles from '../../global.css'
+import gridStyles from 'antd/lib/grid/style/index.css'
+import formStyles from 'antd/lib/form/style/index.css'
+import globalStyles from '../../styles/global.css'
 
 class AppLayout extends React.Component {
   render() {
     return (
       <Layout className={s.container}>
         <Header/>
-        <Layout.Content className={s.content}>
+        <main className={s.content}>
           {this.props.children}
-        </Layout.Content>
-        <Footer/>
+        </main>
       </Layout>
     )
   }
@@ -26,7 +26,8 @@ const mapState = (state) => ({})
 const mapDispatch = {}
 
 export default connect(mapState, mapDispatch)(withStyles(
-  antdStyles,
+  gridStyles,
+  formStyles,
   globalStyles,
   s,
 )(AppLayout))
