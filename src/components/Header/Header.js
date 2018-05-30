5aa6b618d9bf21eb60e75ae6f33e01ef42b69308
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-05-30 03:09:26 +0200

    add svg loader, add logo on header, replace postcss color module with new one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Header.css'
+import {ReactComponent as Logo} from '../../static/logo.svg'
+import {HOME_ROUTE} from '../../routes'
+import {Link} from '../../components'
 
 class Header extends React.Component {
   render() {
     const {currentRouteName} = this.props
     return (
-      <div>
+      <div className={s.header}>
+        <Link to={HOME_ROUTE}>
+          <Logo/>
+        </Link>
       </div>
     )
   }
