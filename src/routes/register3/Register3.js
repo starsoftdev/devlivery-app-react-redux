d3704692c50b2158595a24e2c478ef8029c27731
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-05-30 22:49:12 +0200

    add step 3 on Register flow

--- src/routes/register3/Register3.js
+++ src/routes/register3/Register3.js
@@ -1,70 +1,94 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {clear, register} from '../../reducers/register'
-import {Button, Form, Input} from 'antd'
+import {Button, Col, Form, Input, Row} from 'antd'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Register3.css'
+import ArrowIcon from '../../static/decor_arrow.svg'
+import KeyHandler, {KEYPRESS} from 'react-key-handler'
 import formMessages from '../../formMessages'
+import {setTeamDetails} from '../../reducers/register'
 
 class Register3 extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        // this.props.register(values)
+        this.props.setTeamDetails(values)
       }
     })
   }
 
   render() {
+    const {teamDetails} = this.props
     const {getFieldDecorator} = this.props.form
     return (
-      <main className={s.container}>
-        <div className={s.formWrapper}>
-          <h1 className={s.header}>{'Team Details'}</h1>
-          <Form onSubmit={this.handleSubmit} className={s.form}>
-            <Form.Item>
-              {getFieldDecorator('first_name', {
-                rules: [
-                  {required: true, message: formMessages.required, whitespace: true},
-                ],
-              })(
-                <Input
-                  placeholder={'First Name'}
-                />
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('password', {
-                rules: [
-                  {required: true, message: formMessages.required},
-                ],
-              })(
-                <Input
-                  type='password'
-                  placeholder={'Password'}
-                />
-              )}
-            </Form.Item>
-            <Form.Item className={s.actions}>
-              <Button type='primary' htmlType='submit' className={s.submitBtn}>
-                {'Submit'}
-              </Button>
-            </Form.Item>
-          </Form>
+      <Form onSubmit={this.handleSubmit} className={s.container}>
+        <div className={s.content}>
+          <h1 className={s.header}>
+          <span className={s.headerPrefix}>
+            3
+            <ArrowIcon className={s.arrowIcon}/>
+          </span>
+            Team Details
+          </h1>
+          <Row gutter={20}>
+            <Col xs={24} sm={12}>
+              <Form.Item>
+                {getFieldDecorator('first_name', {
+                  initialValue: teamDetails && teamDetails.first_name,
+                  rules: [
+                    {required: true, message: formMessages.required, whitespace: true},
+                  ],
+                })(
+                  <Input placeholder={'First Name'}/>
+                )}
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item>
+                {getFieldDecorator('last_name', {
+                  initialValue: teamDetails && teamDetails.last_name,
+                  rules: [
+                    {required: true, message: formMessages.required, whitespace: true},
+                  ],
+                })(
+                  <Input placeholder={'Last Name'}/>
+                )}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item>
+            {getFieldDecorator('role', {
+              initialValue: teamDetails && teamDetails.role,
+              rules: [
+                {required: true, message: formMessages.required},
+              ],
+            })(
+              <Input placeholder={'Your Role'}/>
+            )}
+          </Form.Item>
         </div>
-      </main>
+        <div className={s.actions}>
+          <KeyHandler
+            keyEventName={KEYPRESS}
+            keyCode={13}
+            onKeyHandle={this.handleSubmit}
+          />
+          <Button htmlType='submit' type='primary' className={s.submitBtn}>
+            Submit
+          </Button>
+        </div>
+      </Form>
     )
   }
 }
 
 const mapState = state => ({
-  ...state.register,
+  teamDetails: state.register.teamDetails,
 })
 
 const mapDispatch = {
-  register,
-  clear,
+  setTeamDetails,
 }
 
 export default connect(mapState, mapDispatch)(Form.create()(withStyles(s)(Register3)))
