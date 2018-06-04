3ad2cb51620d9005dc973a29339d92cb6da7462e
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-04 12:00:47 +0200

    add right side menu on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,34 @@ import {connect} from 'react-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Header.css'
 import Logo from '../../static/logo.svg'
-import {HOME_ROUTE} from '../../routes'
+import {HOME_ROUTE, LOGIN_ROUTE, REGISTER1_ROUTE, LOGOUT_ROUTE} from '../../routes'
 import {Link} from '../../components'
 import cn from 'classnames'
 
 class Header extends React.Component {
   render() {
-    const {currentRouteName} = this.props
+    const {currentRouteName, user} = this.props
     return (
       <div className={cn(s.header, [HOME_ROUTE].includes(currentRouteName) && s.light)}>
         <Link to={HOME_ROUTE}>
           <Logo/>
         </Link>
+        <div className={s.rightSideMenu}>
+          {!user ? (
+            <React.Fragment>
+              <Link to={LOGIN_ROUTE}>
+                Login
+              </Link>
+              <Link to={REGISTER1_ROUTE}>
+                Sign up
+              </Link>
+            </React.Fragment>
+          ) : (
+            <Link to={LOGOUT_ROUTE}>
+              Sign Out
+            </Link>
+          )}
+        </div>
       </div>
     )
   }
@@ -22,6 +38,7 @@ class Header extends React.Component {
 
 const mapState = state => ({
   currentRouteName: state.global.currentRouteName,
+  user: state.user.user,
 })
 
 const mapDispatch = {}
