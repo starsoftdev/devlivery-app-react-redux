1e14f5f3f11864d7d5c193c4f055a8be0fc46c88
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-20 23:05:07 +0200

    add getting reports, add pagination on Reports table

--- src/routes/reports/index.js
+++ src/routes/reports/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Reports from './Reports'
 import {setCurrentRouteName} from '../../reducers/global'
+import {getReports} from '../../reducers/reports'
 
 function action({query, store, route}) {
   store.dispatch(setCurrentRouteName(route.name))
+  store.dispatch(getReports())
 
   return {
     chunks: ['dashboard'],
