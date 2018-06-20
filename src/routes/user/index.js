9238a3180a43ed0433273c58c7032fc1feffd0a6
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-06-20 23:58:59 +0200

    add initial Account Settings page

---
+++ src/routes/user/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import User from './User'
+import {setCurrentRouteName} from '../../reducers/global'
+
+function action({query, store, route}) {
+  store.dispatch(setCurrentRouteName(route.name))
+
+  return {
+    chunks: ['dashboard'],
+    title: 'Settings',
+    component: <User/>,
+  }
+}
+
+export default action
