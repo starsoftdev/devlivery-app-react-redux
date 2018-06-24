42a26391f6ea4a2a939a3af037c3667f71fdf2cf
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-06-25 01:53:08 +0200

    add translations for Header/Footer

---
+++ src/components/Header/messages.js
@@ -0,0 +1,36 @@
+import {defineMessages} from 'react-intl'
+
+export default defineMessages({
+  login: {
+    id: 'header.login',
+    defaultMessage: 'Login',
+  },
+  signUp: {
+    id: 'header.signUp',
+    defaultMessage: 'Sign Up',
+  },
+  signOut: {
+    id: 'header.signOut',
+    defaultMessage: 'Sign Out',
+  },
+  newArrivals: {
+    id: 'header.newArrivals',
+    defaultMessage: 'New Arrivals',
+  },
+  cardStore: {
+    id: 'header.cardStore',
+    defaultMessage: 'Card Store',
+  },
+  giftStore: {
+    id: 'header.giftStore',
+    defaultMessage: 'Gift Store',
+  },
+  about: {
+    id: 'header.about',
+    defaultMessage: 'About',
+  },
+  contact: {
+    id: 'header.contact',
+    defaultMessage: 'Contact Us',
+  },
+})
