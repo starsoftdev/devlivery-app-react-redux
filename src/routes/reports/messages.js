2ec74ceb30deaf2a066a0024d0d131deefca0874
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-06-25 00:24:40 +0200

    add translations on reports page

---
+++ src/routes/reports/messages.js
@@ -0,0 +1,52 @@
+import {defineMessages} from 'react-intl'
+
+export default defineMessages({
+  title: {
+    id: 'reports.title',
+    defaultMessage: 'Reports',
+  },
+  header: {
+    id: 'reports.header',
+    defaultMessage: 'Schedule',
+  },
+  breadcrumb: {
+    id: 'reports.breadcrumb',
+    defaultMessage: 'Reports',
+  },
+  tableItems: {
+    id: 'reports.tableItems',
+    defaultMessage: '{range0}-{range1} of {total} items',
+  },
+  contactsColumn: {
+    id: 'reports.contactsColumn',
+    defaultMessage: 'Contacts',
+  },
+  scheduledAtColumn: {
+    id: 'reports.scheduledAtColumn',
+    defaultMessage: 'Scheduled At',
+  },
+  itemsColumn: {
+    id: 'reports.itemsColumn',
+    defaultMessage: 'Item(s)',
+  },
+  occasionColumn: {
+    id: 'reports.occasionColumn',
+    defaultMessage: 'Occasion',
+  },
+  sentColumn: {
+    id: 'reports.sentColumn',
+    defaultMessage: 'Sent',
+  },
+  totalColumn: {
+    id: 'reports.totalColumn',
+    defaultMessage: 'Total Price',
+  },
+  export: {
+    id: 'reports.export',
+    defaultMessage: 'Export .XLS',
+  },
+  occasion: {
+    id: 'reports.occasion',
+    defaultMessage: 'Occasion',
+  },
+})
