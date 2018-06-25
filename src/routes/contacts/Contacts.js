57fb93102e45477e782616be2493f1adf1b9fc66
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-25 03:24:25 +0200

    make list view as table on Contacts page

--- src/routes/contacts/Contacts.js
+++ src/routes/contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Col, Input, Pagination, Row, Select} from 'antd'
+import {Col, Input, Pagination, Row, Select, Table} from 'antd'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Contacts.css'
 import EditIcon from '../../static/edit.svg'
@@ -47,6 +47,30 @@ class Contacts extends React.Component {
     // TODO add sort_by
     const {contactsCount, contacts, page, pageSize, loading, getContacts, removeContact, intl} = this.props
 
+    const columns = [
+      {
+        title: intl.formatMessage(messages.nameColumn),
+        dataIndex: '',
+        key: 'name',
+        render: (contact) => `${contact.first_name} ${contact.last_name}`
+      },
+      {
+        title: intl.formatMessage(messages.emailColumn),
+        dataIndex: 'email',
+        key: 'email',
+      },
+      {
+        title: intl.formatMessage(messages.actionsColumn),
+        dataIndex: '',
+        key: 'actions',
+        render: () => {
+          return (
+            <EditIcon/>
+          )
+        }
+      },
+    ]
+
     return (
       <div className={s.container}>
         <div className={s.actions}>
@@ -68,45 +92,60 @@ class Contacts extends React.Component {
             </a>
           </div>
         </div>
-        <Row type='flex' gutter={20}>
-          {contacts.map((contact) =>
-            <Col
-              key={contact.id}
-              {...view === GRID_VIEW ? {
-                xs: 24,
-                sm: 12,
-                md: 6
-              } : {
-                xs: 24
-              }}
-            >
-              <div className={s.contact}>
-                <a className={s.editBtn}>
-                  <EditIcon/>
-                </a>
-                <a className={s.removeBtn} onClick={() => removeContact(contact)}>
-                  <RemoveIcon/>
-                </a>
-                <p className={s.contactName}>{contact.first_name} {contact.last_name}</p>
-                <a href={`tel:${contact.phone}`} className={s.contactPhone}>
-                  {contact.phone}
-                </a>
-              </div>
-            </Col>
-          )}
-        </Row>
-        <div className={s.footer}>
-          <Pagination
-            current={page}
-            total={contactsCount}
-            showTotal={(total, range) => intl.formatMessage(messages.tableItems, {range0: range[0], range1: range[1], total})}
-            pageSize={pageSize}
-            showSizeChanger
-            onChange={(page, pageSize) => getContacts({page, pageSize})}
-            onShowSizeChange={(page, pageSize) => getContacts({page, pageSize})}
-            itemRender={(current, type, el) => <PaginationItem type={type} el={el}/>}
+        {view === GRID_VIEW ? (
+          <React.Fragment>
+            <Row type='flex' gutter={20}>
+              {contacts.map((contact) =>
+                <Col
+                  key={contact.id}
+                  xs={24}
+                  sm={12}
+                  md={6}
+                >
+                  <div className={s.contact}>
+                    <a className={s.editBtn}>
+                      <EditIcon/>
+                    </a>
+                    <a className={s.removeBtn} onClick={() => removeContact(contact)}>
+                      <RemoveIcon/>
+                    </a>
+                    <p className={s.contactName}>{contact.first_name} {contact.last_name}</p>
+                    <a href={`tel:${contact.phone}`} className={s.contactPhone}>
+                      {contact.phone}
+                    </a>
+                  </div>
+                </Col>
+              )}
+            </Row>
+            <div className={s.footer}>
+              <Pagination
+                current={page}
+                total={contactsCount}
+                showTotal={(total, range) => intl.formatMessage(messages.tableItems, {range0: range[0], range1: range[1], total})}
+                pageSize={pageSize}
+                showSizeChanger
+                onChange={(page, pageSize) => getContacts({page, pageSize})}
+                onShowSizeChange={(page, pageSize) => getContacts({page, pageSize})}
+                itemRender={(current, type, el) => <PaginationItem type={type} el={el}/>}
+              />
+            </div>
+          </React.Fragment>
+        ) : (
+          <Table
+            columns={columns}
+            dataSource={contacts}
+            rowKey={record => record.id}
+            onChange={(pagination, filters, sorter) => getContacts({pagination, filters, sorter})}
+            pagination={{
+              current: page,
+              total: contactsCount,
+              showTotal: (total, range) => intl.formatMessage(messages.tableItems, {range0: range[0], range1: range[1], total}),
+              pageSize,
+              showSizeChanger: true,
+              itemRender: (current, type, el) => <PaginationItem type={type} el={el}/>
+            }}
           />
-        </div>
+        )}
       </div>
     )
   }
