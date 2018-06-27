6063482c7a66dd034c87d66e8ab2b648ee7e2348
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-06-28 01:02:28 +0200

    create ContactForm wrapper and reuse it on Purchase step/Dashboard page

--- src/routes/addContacts/messages.js
+++ src/routes/addContacts/messages.js
@@ -5,92 +5,16 @@ export default defineMessages({
     id: 'addContacts.title',
     defaultMessage: 'New Contact',
   },
-  header: {
-    id: 'addContacts.header',
-    defaultMessage: 'Add Contact',
-  },
   breadcrumb: {
     id: 'addContacts.breadcrumb',
     defaultMessage: 'New Contact',
   },
-  salutation: {
-    id: 'addContacts.salutation',
-    defaultMessage: 'Salutation',
-  },
-  firstName: {
-    id: 'addContacts.firstName',
-    defaultMessage: 'First Name',
-  },
-  lastName: {
-    id: 'addContacts.lastName',
-    defaultMessage: 'Last Name',
-  },
-  nickname: {
-    id: 'addContacts.nickname',
-    defaultMessage: 'Nickname',
-  },
-  relationship: {
-    id: 'addContacts.relationship',
-    defaultMessage: 'Relationship',
-  },
-  groups: {
-    id: 'addContacts.groups',
-    defaultMessage: 'Groups',
-  },
-  reminders: {
-    id: 'addContacts.reminders',
-    defaultMessage: 'Reminders',
-  },
-  homeAddress: {
-    id: 'addContacts.homeAddress',
-    defaultMessage: 'Home Address',
-  },
-  companyAddress: {
-    id: 'addContacts.companyAddress',
-    defaultMessage: 'Company Address',
-  },
-  birthday: {
-    id: 'addContacts.birthday',
-    defaultMessage: 'Birthday',
-  },
-  address: {
-    id: 'addContacts.address',
-    defaultMessage: 'Address',
-  },
-  city: {
-    id: 'addContacts.city',
-    defaultMessage: 'City',
-  },
-  state: {
-    id: 'addContacts.state',
-    defaultMessage: 'State',
-  },
-  postalCode: {
-    id: 'addContacts.postalCode',
-    defaultMessage: 'Postal Code',
-  },
-  country: {
-    id: 'addContacts.country',
-    defaultMessage: 'Country',
+  header: {
+    id: 'addContacts.header',
+    defaultMessage: 'Add Contact',
   },
   submit: {
     id: 'addContacts.submit',
     defaultMessage: 'Save Contact',
-  },
-  selectGroup: {
-    id: 'addContacts.selectGroup',
-    defaultMessage: 'Select relevant group',
-  },
-  addGroup: {
-    id: 'addContacts.addGroup',
-    defaultMessage: 'Add new group',
-  },
-  occasion: {
-    id: 'addContacts.occasion',
-    defaultMessage: 'Occasion',
-  },
-  addReminder: {
-    id: 'addContacts.addReminder',
-    defaultMessage: 'Add new reminder',
   },
 })
--- src/routes/purchase10/Purchase10.js
+++ src/routes/purchase10/Purchase10.js
@@ -11,74 +11,69 @@ import KeyHandler, {KEYPRESS} from 'react-key-handler'
 import ImportContacts from './ImportContacts'
 import AddContacts from './AddContacts'
 
+// TODO add Import steps
 class Purchase10 extends React.Component {
   render() {
     const {addingContactsMode, setAddingContactsMode, nextFlowStep, flowIndex} = this.props
     return (
       <React.Fragment>
         {addingContactsMode === ADD_CONTACTS_MANUALLY ? (
-          <div className={s.addContactsContent}>
-            <SectionHeader
-              header={'Add your Contact(s)'}
-              number={flowIndex + 1}
-              prefixClassName={s.addContactsHeaderPrefix}
-            />
-            <AddContacts/>
-          </div>
+          <AddContacts/>
         ) : addingContactsMode === IMPORT_CONTACTS ? (
           <div className={s.importContactsContent}>
-            <SectionHeader
-              header={'Add your Contact(s)'}
-              number={flowIndex + 1}
-              prefixClassName={s.importContactsHeaderPrefix}
-            />
-            <ImportContacts/>
+            {/*<SectionHeader*/}
+              {/*header={'Add your Contact(s)'}*/}
+              {/*number={flowIndex + 1}*/}
+              {/*prefixClassName={s.importContactsHeaderPrefix}*/}
+            {/*/>*/}
+            {/*<ImportContacts/>*/}
           </div>
         ) : (
-          <div className={s.content}>
-            <SectionHeader
-              header={'Add your Contact(s)'}
-              number={flowIndex + 1}
-              prefixClassName={s.headerPrefix}
-            />
-            <Row className={s.items} gutter={20} type='flex' align='center'>
-              <Col className={s.itemWrapper}>
-                <Card
-                  className={s.item}
-                  title={'Add contact manually'}
-                  onClick={() => setAddingContactsMode(ADD_CONTACTS_MANUALLY)}
-                  active={addingContactsMode === ADD_CONTACTS_MANUALLY}
-                  keyValue='a'
-                  svg={AddContactManuallyIcon}
-                />
-              </Col>
-              <Col className={s.itemWrapper}>
-                <Card
-                  className={s.item}
-                  title={'Import contacts'}
-                  onClick={() => setAddingContactsMode(IMPORT_CONTACTS)}
-                  active={addingContactsMode === IMPORT_CONTACTS}
-                  keyValue='b'
-                  svg={ImportContactsIcon}
-                />
-              </Col>
-            </Row>
-          </div>
+          <React.Fragment>
+            <div className={s.content}>
+              <SectionHeader
+                header={'Add your Contact(s)'}
+                number={flowIndex + 1}
+                prefixClassName={s.headerPrefix}
+              />
+              <Row className={s.items} gutter={20} type='flex' align='center'>
+                <Col className={s.itemWrapper}>
+                  <Card
+                    className={s.item}
+                    title={'Add contact manually'}
+                    onClick={() => setAddingContactsMode(ADD_CONTACTS_MANUALLY)}
+                    active={addingContactsMode === ADD_CONTACTS_MANUALLY}
+                    keyValue='a'
+                    svg={AddContactManuallyIcon}
+                  />
+                </Col>
+                <Col className={s.itemWrapper}>
+                  <Card
+                    className={s.item}
+                    title={'Import contacts'}
+                    onClick={() => setAddingContactsMode(IMPORT_CONTACTS)}
+                    active={addingContactsMode === IMPORT_CONTACTS}
+                    keyValue='b'
+                    svg={ImportContactsIcon}
+                  />
+                </Col>
+              </Row>
+            </div>
+            <Actions>
+              <KeyHandler
+                keyEventName={KEYPRESS}
+                keyCode={13}
+                onKeyHandle={nextFlowStep}
+              />
+              <Button
+                onClick={nextFlowStep}
+                type='primary'
+              >
+                Submit
+              </Button>
+            </Actions>
+          </React.Fragment>
         )}
-        <Actions>
-          <KeyHandler
-            keyEventName={KEYPRESS}
-            keyCode={13}
-            onKeyHandle={addingContactsMode && nextFlowStep}
-          />
-          <Button
-            onClick={nextFlowStep}
-            type='primary'
-            disabled={!addingContactsMode}
-          >
-            Submit
-          </Button>
-        </Actions>
       </React.Fragment>
     )
   }
