47218723022f05e11f1ced3530e517919fe285bf
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-27 23:07:41 +0200

    change purchase flow depending on user is logged in

--- src/routes/purchase/index.js
+++ src/routes/purchase/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {AppLayout} from '../../components'
 import Purchase from './Purchase'
+import {setFlow} from '../../reducers/purchase'
+import {AUTH_PURCHASE_ROUTES, PURCHASE_ROUTES} from '../'
+import isEqual from 'lodash/isEqual'
 
-async function action({next, intl}) {
+async function action({store, next, intl}) {
   const child = await next()
+
+  const {flow} = store.getState().purchase
+  const {loggedIn} = store.getState().user
+  store.dispatch(setFlow((isEqual(flow, PURCHASE_ROUTES) && loggedIn) ? AUTH_PURCHASE_ROUTES : PURCHASE_ROUTES))
 
   return {
     chunks: ['purchase'],
