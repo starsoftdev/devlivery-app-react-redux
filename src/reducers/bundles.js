05ee9775053ec4889063832eab5e094ef19f155a
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-29 00:20:14 +0200

    add ability to remove bundle

--- src/reducers/bundles.js
+++ src/reducers/bundles.js
@@ -2,6 +2,7 @@ import createReducer, {RESET_STORE} from '../createReducer'
 import qs from 'query-string'
 import {getToken} from './user'
 import {DEFAULT_PAGE_SIZE} from '../constants'
+import {REMOVE_CONTACT_FAILURE, REMOVE_CONTACT_REQUEST, REMOVE_CONTACT_SUCCESS} from './contacts'
 
 // ------------------------------------
 // Constants
@@ -9,6 +10,10 @@ import {DEFAULT_PAGE_SIZE} from '../constants'
 export const GET_BUNDLES_REQUEST = 'Bundles.GET_BUNDLES_REQUEST'
 export const GET_BUNDLES_SUCCESS = 'Bundles.GET_BUNDLES_SUCCESS'
 export const GET_BUNDLES_FAILURE = 'Bundles.GET_BUNDLES_FAILURE'
+
+export const REMOVE_BUNDLE_REQUEST = 'Bundles.REMOVE_BUNDLE_REQUEST'
+export const REMOVE_BUNDLE_SUCCESS = 'Bundles.REMOVE_BUNDLE_SUCCESS'
+export const REMOVE_BUNDLE_FAILURE = 'Bundles.REMOVE_BUNDLE_FAILURE'
 
 export const CLEAR = 'Bundles.CLEAR'
 
@@ -34,13 +39,30 @@ export const getBundles = (params = {}) => (dispatch, getState, {fetch}) => {
   })
 }
 
+export const removeBundle = (bundle) => (dispatch, getState, {fetch}) => {
+  dispatch({type: REMOVE_BUNDLE_REQUEST})
+  const {token} = dispatch(getToken())
+  return fetch(`/bundles/${bundle.id}`, {
+    method: 'DELETE',
+    token,
+    success: () => {
+      dispatch({type: REMOVE_BUNDLE_SUCCESS})
+      dispatch(getBundles())
+    },
+    failure: () => dispatch({type: REMOVE_BUNDLE_FAILURE}),
+  })
+}
+
 export const clear = () => ({type: CLEAR})
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = {
-  loading: false,
+  loading: {
+    bundles: false,
+    removingBundle: false,
+  },
   bundles: [],
   bundlesCount: 0,
   page: 1,
@@ -53,15 +75,42 @@ export default createReducer(initialState, {
     search: params.search !== undefined ? (params.search || undefined) : state.search,
     page: params.page || 1,
     pageSize: params.pageSize || state.pageSize,
-    loading: true,
+    loading: {
+      ...state.loading,
+      bundles: true,
+    },
   }),
   [GET_BUNDLES_SUCCESS]: (state, {res: {data, meta: {total}}}) => ({
     bundles: data,
     bundlesCount: total,
-    loading: false,
+    loading: {
+      ...state.loading,
+      bundles: false,
+    },
   }),
   [GET_BUNDLES_FAILURE]: (state, action) => ({
-    loading: false,
+    loading: {
+      ...state.loading,
+      bundles: false,
+    },
+  }),
+  [REMOVE_BUNDLE_REQUEST]: (state, action) => ({
+    loading: {
+      ...state.loading,
+      removingBundle: true,
+    },
+  }),
+  [REMOVE_BUNDLE_SUCCESS]: (state, action) => ({
+    loading: {
+      ...state.loading,
+      removingBundle: false,
+    },
+  }),
+  [REMOVE_BUNDLE_FAILURE]: (state, action) => ({
+    loading: {
+      ...state.loading,
+      removingBundle: false,
+    },
   }),
   [CLEAR]: (state, action) => RESET_STORE,
 })
