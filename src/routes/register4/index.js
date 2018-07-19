42d0703afdb2b61c7ed1e625d616bf1acc9d3c0b
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-07-19 23:46:36 +0200

    initial getting permissions and inviting people

--- src/routes/register4/index.js
+++ src/routes/register4/index.js
@@ -3,9 +3,11 @@ import {AppLayout} from '../../components'
 import Register4 from './Register4'
 import {setCurrentRouteName} from '../../reducers/global'
 import messages from './messages'
+import {getPermissions} from '../../reducers/register'
 
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
+  store.dispatch(getPermissions())
 
   return {
     chunks: ['register'],
