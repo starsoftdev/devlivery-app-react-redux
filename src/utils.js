cd05ef51eaf094d238a7f2b4afaed907e443c190
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-07-19 22:48:09 +0200

    make birthday not required on register, fix email error message

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+import {DATE_FORMAT} from './constants'
+
 export const getSuccessMessage = (res) => res && typeof res.data === 'string' ? res.data : null
 
 export const getErrorMessage = (res) => res && typeof res.data === 'string' ? res.data : 'Something went wrong. Please try again.'
+
+export const getBirthday = (birthday) => birthday ? birthday.format(DATE_FORMAT) : null
