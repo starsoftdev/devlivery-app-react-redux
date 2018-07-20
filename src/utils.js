c7b89d8270e56716156f2534aa5478bc3eb81e85
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-07-20 18:31:21 +0200

    add initial sorting for contacts

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,12 @@ export const getSuccessMessage = (res) => res && typeof res.data === 'string' ? 
 export const getErrorMessage = (res) => res && typeof res.data === 'string' ? res.data : 'Something went wrong. Please try again.'
 
 export const getBirthday = (birthday) => birthday ? birthday.format(DATE_FORMAT) : null
+
+export const getOrdering = (ordering) => {
+  const orderDesc = ordering.substring(0, 1) === '-'
+  const sorterField = orderDesc ? ordering.substring(1) : ordering
+  return ordering ? {
+    sort_by: sorterField,
+    ...orderDesc ? {order_by: 'desc'} : {},
+  } : {}
+}
