898329d1d2d73cb0fd079021aca1104a165f3616
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-07-29 02:00:07 +0200

    move title to translations

--- src/routes/addBundle/index.js
+++ src/routes/addBundle/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import AddBundle from './AddBundle'
 import {setCurrentRouteName} from '../../reducers/global'
+import messages from './messages'
 
 async function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
 
   return {
     chunks: ['purchase'],
-    title: 'Add Bundle',
+    title: intl.formatMessage(messages.title),
     component: <AddBundle intl={intl}/>
   }
 }
