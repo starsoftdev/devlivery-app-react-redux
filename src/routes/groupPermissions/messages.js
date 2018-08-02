9b363098c330304f0889d08c7661edcdfefea581
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-08-02 19:09:48 +0300

    permission groups

---
+++ src/routes/groupPermissions/messages.js
@@ -0,0 +1,44 @@
+import {defineMessages} from 'react-intl'
+
+export default defineMessages({
+  title: {
+    id: 'permissionGroups.title',
+    defaultMessage: 'Permission Groups',
+  },
+  header: {
+    id: 'permissionGroups.header',
+    defaultMessage: 'Permission Groups',
+  },
+  breadcrumb: {
+    id: 'permissionGroups.breadcrumb',
+    defaultMessage: 'Permission Groups',
+  },
+  nameColumn: {
+    id: 'permissionGroups.nameColumn',
+    defaultMessage: 'Group Name',
+  },
+  permissionColumn: {
+    id: 'permissionGroups.permissionColumn',
+    defaultMessage: 'Permission Control',
+  },
+  groupName: {
+    id: 'contactGroups.groupName',
+    defaultMessage: 'Group Name',
+  },
+  addGroup: {
+    id: 'contactGroups.addGroup',
+    defaultMessage: 'Add Group',
+  },
+  editPermissions: {
+    id: 'contactGroups.editPermissions',
+    defaultMessage: 'Edit Permissions',
+  },
+  savePermissions: {
+    id: 'contactGroups.savePermissions',
+    defaultMessage: 'Save Permissions',
+  },
+  deleteGroup: {
+    id: 'contactGroups.deleteGroup',
+    defaultMessage: 'Delete Group',
+  }
+})
