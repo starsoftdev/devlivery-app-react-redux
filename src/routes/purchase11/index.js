62afd053edbff274909b9f7fc73e9c7eb2d1a2bd
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-09-11 17:14:46 +0300

    saved bundle

--- src/routes/purchase11/index.js
+++ src/routes/purchase11/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Purchase11 from './Purchase11'
 import {setCurrentRouteName} from '../../reducers/global'
 import {makeOrder} from '../../reducers/purchase'
+import {getUserDetails} from '../../reducers/user'
 
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
+  store.dispatch(getUserDetails())
   store.dispatch(makeOrder())
 
   return {
