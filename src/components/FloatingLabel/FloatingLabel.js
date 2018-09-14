306dfe642ac47b600ae76e683f60ac2e38394183
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-14 20:51:13 +0300

    customize floating-label component

---
+++ src/components/FloatingLabel/FloatingLabel.js
@@ -0,0 +1,59 @@
+import React, {PropTypes} from 'react';
+import classNames from 'classnames';
+import {Input} from 'antd'
+
+export default class FloatingLabel extends React.Component {
+  static propTypes: {
+    autoComplete: PropTypes.bool,
+    errorMsg: PropTypes.string,
+    placeholder: PropTypes.string.isRequired,
+    pattern: PropTypes.any,
+    type: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    isDisabled: PropTypes.bool
+  };
+
+  constructor (props) {
+    super(props)
+    this.state = {hasValue: false, hasError: false};
+  }
+
+  onBlur(event) {
+    this.setState({hasValue: Boolean(event.currentTarget.value)});
+  }
+
+  onChange(event) {
+    const {pattern} = this.props;
+    this.setState({
+      hasError: !pattern.test(event.currentTarget.value),
+      hasValue: Boolean(event.currentTarget.value)
+    });
+  }
+
+  render () {
+    const {autoComplete, errorMsg, id, isDisabled, pattern, placeholder, type, value} = this.props;
+    const {hasValue, hasError} = this.state;
+    const inputClasses = classNames('fl-input', {'fl-valid': hasValue && !hasError}, {'fl-invalid': hasValue && hasError});
+    
+    return(
+      <div className='fl-input-container'>
+        <Input
+          autoComplete={autoComplete}
+          className={inputClasses}
+          disabled={isDisabled}
+          onBlur={this.onBlur.bind(this)}
+          onChange={this.props.onChange}
+          type={type}/>
+        <label className='fl-input-label' htmlFor={id}>{placeholder}</label>
+        <span className='fl-input-bar'></span>
+      </div>
+    );
+  }
+}
+
+FloatingLabel.defaultProps = {
+  autoComplete: 'false',
+  type: 'text',
+  isDisabled: false,
+  placeholder: 'name'
+};
