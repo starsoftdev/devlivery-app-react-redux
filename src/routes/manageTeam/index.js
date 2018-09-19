9cf0f00ec61003f5867c9432bd9d9485b7a8191c
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-19 17:40:46 +0300

    add /user-permissions and /has-any-permission

--- src/routes/manageTeam/index.js
+++ src/routes/manageTeam/index.js
@@ -5,12 +5,14 @@ import {getTeam} from '../../reducers/team'
 import {getPendingTeam} from '../../reducers/pendingMembers'
 import messages from './messages'
 import {TEAM_ACCOUNT} from '../../reducers/register'
+import {getUserPermission,hasAnyPermission} from '../../reducers/permissions'
 
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
   store.dispatch(getTeam())
   store.dispatch(getPendingTeam())
-
+  //store.dispatch(hasAnyPermission(["Can pay"]))
+  
   const {user} = store.getState().user
 
   if (user.account_type !== TEAM_ACCOUNT) {
