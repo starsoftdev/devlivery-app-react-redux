1b04d98d37a2c6c82b84bd699c42db3b9c6c17c4
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-10-08 12:14:50 +0300

    add updateordermeta api

--- src/routes/purchase12/index.js
+++ src/routes/purchase12/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Purchase12 from './Purchase12'
 import {setCurrentRouteName} from '../../reducers/global'
-
+import {updateOrderMeta} from '../../reducers/purchase';
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
-
+  store.dispatch(updateOrderMeta())
   return {
     chunks: ['purchase'],
     title: 'Purchase',
