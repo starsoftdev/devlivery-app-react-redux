90711cb839d3dfa3173d403cdc3a727502267445
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-10-09 18:58:30 +0300

    apply payment permission on purchase flow

--- src/routes/purchase12/index.js
+++ src/routes/purchase12/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Purchase12 from './Purchase12'
 import {setCurrentRouteName} from '../../reducers/global'
 import {updateOrderMeta} from '../../reducers/purchase';
+import {getUserPermission} from '../../reducers/permissions';
+
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
   store.dispatch(updateOrderMeta())
+  store.dispatch(getUserPermission())
   return {
     chunks: ['purchase'],
     title: 'Purchase',
