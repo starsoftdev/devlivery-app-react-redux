e5fdd3c69da60fca4b5b519aa3eb11adb4fe6de5
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-10-16 14:55:55 +0300

    remove donation and voucher

--- src/components/OrderItems/OrderItems.js
+++ src/components/OrderItems/OrderItems.js
@@ -1,40 +1,52 @@
 import React from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './OrderItems.css'
-import {CARD_IMAGES_PROP, GIFT_IMAGES_PROP} from '../../constants'
-import {getItemImage} from '../../utils'
+import { CARD_IMAGES_PROP, GIFT_IMAGES_PROP } from '../../constants'
+import { getItemImage } from '../../utils'
 import PlusGiftIcon from '../../static/plus_round.svg'
+import RemoveIcon from '../../static/remove.svg'
+import { Popconfirm } from 'antd'
+import messages from './messages'
 
 class OrderItems extends React.Component {
   render() {
-    const {voucher, donation, card, gift,giftcount} = this.props
+    const { intl, voucher, donation, card, gift, giftcount, removeDontationFromBundle, removeVoucherFromBundle } = this.props
 
     return (
       <div className={s.orderInfo}>
         <div className={s.cardWrapper}>
           <div
-            style={{backgroundImage: `url(${getItemImage(card, CARD_IMAGES_PROP)})`}}
+            style={{ backgroundImage: `url(${getItemImage(card, CARD_IMAGES_PROP)})` }}
             className={s.itemImage}
           />
           {(gift || voucher || donation) && (
-            <PlusGiftIcon className={s.plusIcon}/>
+            <PlusGiftIcon className={s.plusIcon} />
           )}
           <p className={s.cardInfo}>
             <span className={s.cardType}>{card.title}</span>
-            <br/>
+            <br />
             <span className={s.cardPrice}>{card.price}</span>
             <span className={s.cardPriceCurrency}>{card.currency}</span>
           </p>
         </div>
         {gift && (
           <div className={s.cardWrapper}>
+            <Popconfirm
+              title={intl.formatMessage(messages.confirmRemoving)}
+              onConfirm={() => {}}
+              okText={intl.formatMessage(messages.acceptRemoving)}
+            >
+              <a className={s.removeIcon}>
+                <RemoveIcon />
+              </a>
+            </Popconfirm>
             <div
-              style={{backgroundImage: `url(${getItemImage(gift, GIFT_IMAGES_PROP)})`}}
+              style={{ backgroundImage: `url(${getItemImage(gift, GIFT_IMAGES_PROP)})` }}
               className={s.itemImage}
             />
             <p className={s.cardInfo}>
-              <span className={s.cardType}>{"GIFT ("+giftcount+")"}</span>
-              <br/>
+              <span className={s.cardType}>{"GIFT (" + giftcount + ")"}</span>
+              <br />
               <span className={s.cardPrice}>{gift.price}</span>
               <span className={s.cardPriceCurrency}>{gift.currency}</span>
             </p>
@@ -42,13 +54,22 @@ class OrderItems extends React.Component {
         )}
         {voucher && (
           <div className={s.cardWrapper}>
+            <Popconfirm
+              title={intl.formatMessage(messages.confirmRemoving)}
+              onConfirm={() => removeVoucherFromBundle()}
+              okText={intl.formatMessage(messages.acceptRemoving)}
+            >
+              <a className={s.removeIcon}>
+                <RemoveIcon />
+              </a>
+            </Popconfirm>
             <div>
-              <span className={s.cardType}>From: {voucher.from}</span><br/>
+              <span className={s.cardType}>From: {voucher.from}</span><br />
               <span className={s.cardType}>To: {voucher.to}</span>
             </div>
             <p className={s.cardInfo}>
               <span className={s.cardType}>{voucher.title}</span>
-              <br/>
+              <br />
               <span className={s.cardPrice}>{voucher.price}</span>
               <span className={s.cardPriceCurrency}>{'CHF'}</span>
             </p>
@@ -56,13 +77,23 @@ class OrderItems extends React.Component {
         )}
         {donation && (
           <div className={s.cardWrapper}>
+            <Popconfirm
+              title={intl.formatMessage(messages.confirmRemoving)}
+              onConfirm={() => removeDontationFromBundle()}
+              okText={intl.formatMessage(messages.acceptRemoving)}
+            >
+              <a className={s.removeIcon}>
+                <RemoveIcon />
+              </a>
+            </Popconfirm>
+
             <div
-              style={{backgroundImage: `url(${getItemImage(donation.organization, 'logo')})`}}
+              style={{ backgroundImage: `url(${getItemImage(donation.organization, 'logo')})` }}
               className={s.itemImage}
             />
             <p className={s.cardInfo}>
               <span className={s.cardType}>{donation.organization.name}</span>
-              <br/>
+              <br />
               <span className={s.cardPrice}>{donation.amount}</span>
               <span className={s.cardPriceCurrency}>{'CHF'}</span>
             </p>
@@ -75,7 +106,7 @@ class OrderItems extends React.Component {
             />
             <p className={s.cardInfo}>
               <span className={s.cardType}></span>
-              <br/>
+              <br />
               <span className={s.cardPrice}></span>
               <span className={s.cardPriceCurrency}></span>
             </p>
