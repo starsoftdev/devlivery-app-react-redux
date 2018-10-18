4e5431be4626ae966992600eacd176437b9e5cbf
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-10-18 18:14:26 +0300

    replace URLSearchParams

--- src/routes/paypalRedirectBack/index.js
+++ src/routes/paypalRedirectBack/index.js
@@ -3,13 +3,13 @@ import PaypalRedirectBack from './PaypalRedirectBack'
 import {setCurrentRouteName} from '../../reducers/global'
 import {PAYPAL_REDIRECT_BACK_CANCEL} from '../../routes'
 
-function action({store, route, intl}) {
+function action({store, route, intl, query}) {
   store.dispatch(setCurrentRouteName(route.name))
 
   return {
     chunks: ['purchase'],
     title: 'PayPal Redirect',
-    component: <PaypalRedirectBack cancel={route.name === PAYPAL_REDIRECT_BACK_CANCEL} intl={intl}/>
+    component: <PaypalRedirectBack cancel={route.name === PAYPAL_REDIRECT_BACK_CANCEL} intl={intl} query={query}/>
   }
 }
 
