5de051b42710372b2480254d768ff146972966a9
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-10-23 14:44:37 +0300

    Flow > Payment > Improve UI on the payment with credit card view

--- src/routes/purchase13/index.js
+++ src/routes/purchase13/index.js
@@ -3,10 +3,12 @@ import Purchase13 from './Purchase13'
 import {setCurrentRouteName} from '../../reducers/global'
 import messages from './messages'
 import {getAllCards} from '../../reducers/user'
+import {makeOrder} from '../../reducers/purchase'
 
 function action({store, route, intl}) {
   store.dispatch(setCurrentRouteName(route.name))
   store.dispatch(getAllCards())
+  store.dispatch(makeOrder())
   return {
     chunks: ['purchase'],
     title: intl.formatMessage(messages.title),
