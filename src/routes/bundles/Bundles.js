e9e426d0c448458fa70c8039a95d82948575b9bc
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-11-05 17:55:27 +0200

    Bundle view multiple products selected

--- src/routes/bundles/Bundles.js
+++ src/routes/bundles/Bundles.js
@@ -1,25 +1,101 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {Button, Col, Input, Pagination, Row, Popconfirm} from 'antd'
+import { connect } from 'react-redux'
+import { Button, Col, Input, Pagination, Row, Popconfirm } from 'antd'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Bundles.css'
-import {clear, getBundles, openBundleDetailsModal, removeBundle} from '../../reducers/bundles'
+import { clear, getBundles, openBundleDetailsModal, removeBundle } from '../../reducers/bundles'
 import PlusGiftIcon from '../../static/plus_round.svg'
 import PlusIcon from '../../static/plus.svg'
-import {BundleDetails, PaginationItem} from '../../components'
+import { BundleDetails, PaginationItem } from '../../components'
 import debounce from 'lodash/debounce'
 import messages from './messages'
-import {CARD_IMAGES_PROP, DEFAULT_DEBOUNCE_TIME, GIFT_IMAGES_PROP} from '../../constants'
-import {EDIT_BUNDLE_FLOW} from '../'
-import {setBundle, setFlow, setSavedValue} from '../../reducers/purchase'
+import { CARD_IMAGES_PROP, DEFAULT_DEBOUNCE_TIME, GIFT_IMAGES_PROP } from '../../constants'
+import { EDIT_BUNDLE_FLOW } from '../'
+import { setBundle, setFlow, setSavedValue } from '../../reducers/purchase'
 import RemoveIcon from '../../static/remove.svg'
-import {getItemImage} from '../../utils'
+import { getItemImage } from '../../utils'
+import LeftIcon from '../../static/left-arrow.svg'
+import RightIcon from '../../static/right-arrow.svg'
 
+class BundleItem extends React.Component {
+  state = {
+    giftIndex: 0
+  }
+  onclickLeft(e) {
+    e.stopPropagation();
+    const {giftIndex} = this.state;
+    const gifts = this.props.bundle.bundle_gifts;
+    this.setState({giftIndex: giftIndex > 0 ? (giftIndex-1):(gifts.length-1)});
+  }
+  onclickRight(e) {
+    e.stopPropagation();
+    const {giftIndex} = this.state;
+    const gifts = this.props.bundle.bundle_gifts;
+    this.setState({giftIndex: giftIndex < (gifts.length-1) ? (giftIndex+1):0});
+  }
+  render() {
+    const {bundle,intl,openBundleDetailsModal,removeBundle,setBundle} = this.props;
+    
+    return (
+      <Col xs={24} md={12}>
+        <div className={s.bundle}>
+          <Popconfirm
+            title={intl.formatMessage(messages.confirmRemoving)}
+            onConfirm={() => removeBundle(bundle)}
+            okText={intl.formatMessage(messages.acceptRemoving)}
+          >
+            <a className={s.removeBtn} >
+              <RemoveIcon />
+            </a>
+          </Popconfirm>
+          <div className={s.bundleContent} onClick={() => openBundleDetailsModal(bundle)}>
+            <div className={s.cardWrapper}>
+              <div style={{ backgroundImage: `url(${getItemImage(bundle.bundle_card.card, CARD_IMAGES_PROP)})` }} className={s.itemImage} />
+              {bundle.bundle_gifts[0] && (
+                <PlusGiftIcon className={s.plusIcon} />
+              )}
+            </div>
+            {bundle.bundle_gifts[this.state.giftIndex] && (
+              <div className={s.giftWrapper}>
+                <div style={{ backgroundImage: `url(${getItemImage(bundle.bundle_gifts[this.state.giftIndex].gift, GIFT_IMAGES_PROP)})` }} className={s.itemImage} />
+                {
+                  bundle.bundle_gifts && bundle.bundle_gifts.length > 1 &&
+                  <div className={s.buttonBar}>
+                    <div className={s.leftIcon} onClick={(e)=>this.onclickLeft(e)}>
+                      <LeftIcon />
+                    </div>
+                    <div className={s.rightIcon} onClick={(e)=>this.onclickRight(e)}>
+                      <RightIcon />
+                    </div>
+                  </div>
+                }
+              </div>
+            )}
+          </div>
+          <div>
+            <div className={s.bundleActions}>
+              <div className={s.cardInfo}>
+                <a className={s.cardTitle} onClick={() => openBundleDetailsModal(bundle)}>{bundle.title}</a>
+                <br />
+                <span className={s.cardPrice}>{bundle.total}</span>
+                <span className={s.cardPriceCurrency}>CHF</span>
+              </div>
+              <Button type='primary' ghost onClick={() => setBundle(bundle)}>
+                <PlusIcon />
+                {intl.formatMessage(messages.makeOrder)}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Col>
+    );
+  }
+}
 class Bundles extends React.Component {
   changeSearch = (e) => {
     const search = e.target.value
-    this.setState({search})
-    this.getBundles({search})
+    this.setState({ search })
+    this.getBundles({ search })
   }
 
   constructor(props) {
@@ -37,10 +113,10 @@ class Bundles extends React.Component {
   }
 
   render() {
-    const {search} = this.state
+    const { search } = this.state
     // TODO add loading
-    const {bundlesCount, bundles, page, pageSize, loading, getBundles, intl, setBundle, setFlow, setSavedValue, removeBundle, openBundleDetailsModal, bundleDetailsModalOpened} = this.props
-    
+    const { bundlesCount, bundles, page, pageSize, loading, getBundles, intl, setBundle, setFlow, setSavedValue, removeBundle, openBundleDetailsModal, bundleDetailsModalOpened } = this.props
+
     return (
       <div className={s.container}>
         <div className={s.actions}>
@@ -55,52 +131,20 @@ class Bundles extends React.Component {
             setFlow(EDIT_BUNDLE_FLOW)
             setSavedValue(1);
           }}>
-            <PlusIcon/>
+            <PlusIcon />
             {intl.formatMessage(messages.addBundle)}
           </Button>
         </div>
         <Row type='flex' gutter={20}>
           {bundles.map((bundle) =>
-            <Col key={bundle.id} xs={24} md={12}>
-              <div className={s.bundle}>
-                <Popconfirm
-                  title={intl.formatMessage(messages.confirmRemoving)}
-                  onConfirm={() => removeBundle(bundle)}
-                  okText={intl.formatMessage(messages.acceptRemoving)}
-                >
-                  <a className={s.removeBtn} >
-                    <RemoveIcon/>
-                  </a>
-                </Popconfirm>
-                <a className={s.bundleContent} onClick={() => openBundleDetailsModal(bundle)}>
-                  <div className={s.cardWrapper}>
-                    <div style={{backgroundImage: `url(${getItemImage(bundle.bundle_card.card, CARD_IMAGES_PROP)})`}} className={s.itemImage}/>
-                    {bundle.bundle_gifts[0] && (
-                      <PlusGiftIcon className={s.plusIcon}/>
-                    )}
-                  </div>
-                  {bundle.bundle_gifts[0] && (
-                    <div className={s.giftWrapper}>
-                      <div style={{backgroundImage: `url(${getItemImage(bundle.bundle_gifts[0].gift, GIFT_IMAGES_PROP)})`}} className={s.itemImage}/>
-                    </div>
-                  )}
-                </a>
-                <div>
-                  <div className={s.bundleActions}>
-                    <div className={s.cardInfo}>
-                      <a className={s.cardTitle} onClick={() => openBundleDetailsModal(bundle)}>{bundle.title}</a>
-                      <br/>
-                      <span className={s.cardPrice}>{bundle.total}</span>
-                      <span className={s.cardPriceCurrency}>CHF</span>
-                    </div>
-                    <Button type='primary' ghost onClick={() => setBundle(bundle)}>
-                      <PlusIcon/>
-                      {intl.formatMessage(messages.makeOrder)}
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </Col>
+            <BundleItem 
+              key={bundle.id}
+              bundle = {bundle}
+              intl = {intl}
+              openBundleDetailsModal = {openBundleDetailsModal}
+              removeBundle = {removeBundle}
+              setBundle = {setBundle}
+              />
           )}
         </Row>
         <div className={s.footer}>
@@ -114,12 +158,12 @@ class Bundles extends React.Component {
             })}
             pageSize={pageSize}
             showSizeChanger
-            onChange={(page, pageSize) => getBundles({page, pageSize})}
-            onShowSizeChange={(page, pageSize) => getBundles({page, pageSize})}
-            itemRender={(current, type, el) => <PaginationItem type={type} el={el}/>}
+            onChange={(page, pageSize) => getBundles({ page, pageSize })}
+            onShowSizeChange={(page, pageSize) => getBundles({ page, pageSize })}
+            itemRender={(current, type, el) => <PaginationItem type={type} el={el} />}
           />
         </div>
-        {bundleDetailsModalOpened && <BundleDetails/>}
+        {bundleDetailsModalOpened && <BundleDetails />}
       </div>
     )
   }
