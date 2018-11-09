bf6af72e8bce6ac8b59a77e88bb5ac9232f244d9
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-11-09 16:20:50 +0200

    move about/contactus menu to left of logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Header.css'
 import Logo from '../../static/logo.svg'
@@ -17,14 +17,14 @@ import {
   REGISTER1_ROUTE,
   CONTACT_US_ROUTE,
 } from '../../routes'
-import {LanguageSwitcher, Link} from '../../components'
+import { LanguageSwitcher, Link } from '../../components'
 import cn from 'classnames'
-import {injectIntl} from 'react-intl'
+import { injectIntl } from 'react-intl'
 import messages from './messages'
 
 class Header extends React.Component {
   render() {
-    const {className, currentRouteName, user, intl} = this.props
+    const { className, currentRouteName, user, intl } = this.props
     return (
       <header
         className={cn(
@@ -40,62 +40,74 @@ class Header extends React.Component {
             <LanguageSwitcher/>
           </div>
           */}
+          {[
+            ...HOME_ROUTES,
+            ...DASHBOARD_ROUTES,
+          ].includes(currentRouteName) && (
+              <div className={s.leftMenu}>
+                <React.Fragment>
+                  <Link to={ABOUT_ROUTE}>{intl.formatMessage(messages.about)}</Link>
+                  <Link to={CONTACT_US_ROUTE}>{intl.formatMessage(messages.contact)}</Link>
+                </React.Fragment>
+              </div>
+            )}
           <Link to={HOME_ROUTE}>
-            <Logo/>
+            <Logo />
           </Link>
           {[
             ...HOME_ROUTES,
             ...DASHBOARD_ROUTES,
           ].includes(currentRouteName) && (
-            <div className={s.rightMenu}>
-              {!user ? (
-                <React.Fragment>
-                  <Link to={LOGIN_ROUTE}>
-                    {intl.formatMessage(messages.login)}
-                  </Link>
-                  <Link to={REGISTER1_ROUTE}>
-                    {intl.formatMessage(messages.signUp)}
-                  </Link>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  <Link to={LOGOUT_ROUTE}>
-                    {intl.formatMessage(messages.signOut)}
-                  </Link>
-                  <Link to={ORDERS_ROUTE}>
-                    {intl.formatMessage(messages.dashboard)}
-                  </Link>
-                </React.Fragment>
-              )}
-            </div>
-          )}
+              <div className={s.rightMenu}>
+                {!user ? (
+                  <React.Fragment>
+                    <Link to={LOGIN_ROUTE}>
+                      {intl.formatMessage(messages.login)}
+                    </Link>
+                    <Link to={REGISTER1_ROUTE}>
+                      {intl.formatMessage(messages.signUp)}
+                    </Link>
+                  </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                      <Link to={LOGOUT_ROUTE}>
+                        {intl.formatMessage(messages.signOut)}
+                      </Link>
+                      <Link to={ORDERS_ROUTE}>
+                        {intl.formatMessage(messages.dashboard)}
+                      </Link>
+                    </React.Fragment>
+                  )}
+              </div>
+            )}
         </div>
         {[
           ...HOME_ROUTES,
           ...DASHBOARD_ROUTES,
         ].includes(currentRouteName) && (
-          <ul className={s.bottomMenu}>
-            <li onClick={this.getLocation}>
-              <Link to={NEW_ARRIVALS_ROUTE}>{intl.formatMessage(messages.newArrivals)}</Link>
-              {NEW_ARRIVALS_ROUTE === currentRouteName &&
-              <div className={s.dashWrapper}>
-                <hr className={s.underline}/>
-              </div>}
-            </li>
-            <li>
-              <Link to={CARD_STORE_ROUTE}>{intl.formatMessage(messages.cardStore)}</Link>
-              {CARD_STORE_ROUTE === currentRouteName &&
-              <div className={s.dashWrapper}>
-                <hr className={s.underline}/>
-              </div>}
-            </li>
-            <li>
-              <Link to={GIFT_STORE_ROUTE}>{intl.formatMessage(messages.giftStore)}</Link>
-              {GIFT_STORE_ROUTE === currentRouteName &&
-              <div className={s.dashWrapper}>
-                <hr className={s.underline}/>
-              </div>}
-            </li>
+            <ul className={s.bottomMenu}>
+              <li onClick={this.getLocation}>
+                <Link to={NEW_ARRIVALS_ROUTE}>{intl.formatMessage(messages.newArrivals)}</Link>
+                {NEW_ARRIVALS_ROUTE === currentRouteName &&
+                  <div className={s.dashWrapper}>
+                    <hr className={s.underline} />
+                  </div>}
+              </li>
+              <li>
+                <Link to={CARD_STORE_ROUTE}>{intl.formatMessage(messages.cardStore)}</Link>
+                {CARD_STORE_ROUTE === currentRouteName &&
+                  <div className={s.dashWrapper}>
+                    <hr className={s.underline} />
+                  </div>}
+              </li>
+              <li>
+                <Link to={GIFT_STORE_ROUTE}>{intl.formatMessage(messages.giftStore)}</Link>
+                {GIFT_STORE_ROUTE === currentRouteName &&
+                  <div className={s.dashWrapper}>
+                    <hr className={s.underline} />
+                  </div>}
+              </li>
+              {/*
             <li>
               <Link to={ABOUT_ROUTE}>{intl.formatMessage(messages.about)}</Link>
               {ABOUT_ROUTE === currentRouteName &&
@@ -110,8 +122,9 @@ class Header extends React.Component {
                 <hr className={s.underline}/>
               </div>}
             </li>
-          </ul>
-        )}
+            */}
+            </ul>
+          )}
       </header>
     )
   }
