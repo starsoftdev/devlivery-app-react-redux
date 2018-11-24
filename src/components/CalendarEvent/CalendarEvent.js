fe0751744956a383f0f5f7d450d5cc4695775c1a
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-11-24 11:53:53 +0200

    use 'reminder' date instead of 'contact_specific_date'

--- src/components/CalendarEvent/CalendarEvent.js
+++ src/components/CalendarEvent/CalendarEvent.js
@@ -8,11 +8,11 @@ import LongArrowIcon from '../../static/long-right-arrow.svg'
 import {DATE_FORMAT} from '../../constants'
 import {setFlow} from '../../reducers/purchase'
 
-const CalendarEvent = ({first_name, last_name, occasion, contact_specific_date, occasion_date, occasion_type, setFlow}) =>
+const CalendarEvent = ({first_name, last_name, occasion, reminder_date, occasion_date, occasion_type, setFlow}) =>
   <a className={s.event} onClick={() => {/*setFlow(EVENT_PURCHASE_FLOW)*/}}>
     <div className={s.eventDate}>
-      <div className={s.eventDay}>{moment(contact_specific_date || occasion_date, DATE_FORMAT).format('D')}</div>
-      <div className={s.eventMonth}>{moment(contact_specific_date || occasion_date, DATE_FORMAT).format('MMMM')}</div>
+      <div className={s.eventDay}>{moment(reminder_date || occasion_date, DATE_FORMAT).format('D')}</div>
+      <div className={s.eventMonth}>{moment(reminder_date || occasion_date, DATE_FORMAT).format('MMMM')}</div>
     </div>
     <div className={s.eventDetails}>
       <div className={s.eventType}>{occasion} ({occasion_type})</div>
