da6730135dcf2ca7e8aa3148602e7d3e278cb8d8
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-11-26 12:36:11 +0200

    show title for custom reminder

--- src/components/CalendarEvent/CalendarEvent.js
+++ src/components/CalendarEvent/CalendarEvent.js
@@ -8,14 +8,14 @@ import LongArrowIcon from '../../static/long-right-arrow.svg'
 import {DATE_FORMAT} from '../../constants'
 import {setFlow} from '../../reducers/purchase'
 
-const CalendarEvent = ({first_name, last_name, occasion, reminder_date, occasion_date, occasion_type, setFlow}) =>
+const CalendarEvent = ({first_name, last_name, occasion, reminder_date, occasion_date, occasion_type, setFlow, custom_title}) =>
   <a className={s.event} onClick={() => {/*setFlow(EVENT_PURCHASE_FLOW)*/}}>
     <div className={s.eventDate}>
       <div className={s.eventDay}>{moment(reminder_date || occasion_date, DATE_FORMAT).format('D')}</div>
       <div className={s.eventMonth}>{moment(reminder_date || occasion_date, DATE_FORMAT).format('MMMM')}</div>
     </div>
     <div className={s.eventDetails}>
-      <div className={s.eventType}>{occasion} ({occasion_type})</div>
+      <div className={s.eventType}>{occasion ? occasion : custom_title} ({occasion ? occasion_type : 'custom'})</div>
       <div className={s.eventName}>{first_name} {last_name}</div>
       {/*<LongArrowIcon className={s.eventArrowIcon}/>*/}
     </div>
