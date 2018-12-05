fba41bdb76256feb4d8f142f6784529d8464eb33
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-12-05 14:29:46 +0200

    fix dob issue on purchase flow

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ module.exports = {
   port: process.env.PORT || 80,
 
   api: {
-    url: process.env.API_URL || 'http://139.59.175.153:8081/api/v1',//'http://byzumi-dev.colorelephant.com:8081/api/v1'
+    url: process.env.API_URL || 'http://byzumi-dev.colorelephant.com:8081/api/v1'
   },
 
   stripe: {
@@ -30,5 +30,5 @@ module.exports = {
     googleTrackingId: process.env.GOOGLE_TRACKING_ID || '',
   },
 
-  CRISP_WEBSITE_ID : "1a110172-9bbb-4d34-9ce2-7e30d1bbc9a0" //"a923a36b-3c6f-4d91-972c-d37c851ed6d8" (byzumi-dev)
+  CRISP_WEBSITE_ID : "a923a36b-3c6f-4d91-972c-d37c851ed6d8"
 }
