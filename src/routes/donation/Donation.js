2fa0825df8fd9524ed56aa97892ae2745287a0e3
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-12-18 09:50:57 +0200

    Changes to layout donations

--- src/routes/donation/Donation.js
+++ src/routes/donation/Donation.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { submitDonation, setDonationOrg,getDonationOrgs,DONATION_STATE,removeDontationFromBundle } from '../../reducers/purchase'
+import { submitDonation, setDonationOrg, getDonationOrgs, DONATION_STATE, removeDontationFromBundle } from '../../reducers/purchase'
 import { Button, Col, Form, Input, Row, Checkbox, Pagination, message } from 'antd'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Donation.css'
-import { Actions, Card, PurchaseActions, SectionHeader,PaginationItem } from '../../components'
+import { Actions, Card, PurchaseActions, SectionHeader, PaginationItem } from '../../components'
 import { ALPHABET } from '../../constants'
 import KeyHandler, { KEYPRESS } from 'react-key-handler'
 import messages from './messages'
@@ -17,22 +17,19 @@ class Donation extends React.Component {
     amountValue: '',
     hideAmount: false
   }
-  constructor(props){
+  constructor(props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleSubmit = (e,refresh=false) => {
+  handleSubmit = (e, refresh = false) => {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
-      if(this.state.donationOrg)
-      {
-        if(!err)
-        {
+      if (this.state.donationOrg) {
+        if (!err) {
           let errmsg = null;
-          if(values['donationAmount'] < 0){errmsg = 'This field must be positive.';}
-          if(values['donationAmount'] == 0){errmsg = 'Sorry, but donation needs to be bigger than 0';}
-          if(errmsg)
-          {
+          if (values['donationAmount'] < 0) { errmsg = 'This field must be positive.'; }
+          if (values['donationAmount'] == 0) { errmsg = 'Sorry, but donation needs to be bigger than 0'; }
+          if (errmsg) {
             this.props.form.setFields({
               donationAmount: {
                 value: values['donationAmount'],
@@ -43,16 +40,15 @@ class Donation extends React.Component {
           }
           this.props.setDonationOrg(this.state.donationOrg);
           localStorage.setItem(DONATION_STATE, JSON.stringify(values));
-          this.props.submitDonation(values,refresh)
+          this.props.submitDonation(values, refresh)
         }
       }
       else {
-        if(refresh)
-        {
+        if (refresh) {
           this.props.setDonationOrg(null);
           localStorage.removeItem(DONATION_STATE);
           this.props.removeDontationFromBundle()
-          this.props.submitDonation(null,refresh)
+          this.props.submitDonation(null, refresh)
         }
       }
     })
@@ -63,7 +59,7 @@ class Donation extends React.Component {
   async loadLocalStorage() {
     var initState = await localStorage.getItem(DONATION_STATE);
     initState = JSON.parse(initState);
-    this.setState({ ...initState, donationOrg: this.props.donationOrg, init_donation: this.props.donationOrg});
+    this.setState({ ...initState, donationOrg: this.props.donationOrg, init_donation: this.props.donationOrg });
   }
   render() {
     const { setDonationOrg, donationOrgs, intl, flowIndex, loading, donationAmount, hideAmount, doCount, doPage, doPageSize, getDonationOrgs } = this.props
@@ -73,12 +69,36 @@ class Donation extends React.Component {
     return (
       <React.Fragment>
         <div className={s.content}>
-          <SectionHeader
-            header={intl.formatMessage(messages.header)}
-            number={flowIndex + 1}
-            prefixClassName={s.headerPrefix}
-          />
+          <Row>
+            <Col xs={18}>
+              <SectionHeader
+                header={intl.formatMessage(messages.header)}
+                number={flowIndex + 1}
+                prefixClassName={s.headerPrefix}
+              />
+            </Col>
+            <Col xs={6}>
+              {
+                donationOrg &&
+                <div className={s.removeBtn}>
+                  <Button
+                    type='primary'
+                    size={'default'}
+                    onClick={() => {
+                      this.props.setDonationOrg(null);
+                      localStorage.removeItem(DONATION_STATE);
+                      this.props.removeDontationFromBundle()
+                      this.props.submitDonation(null, true)
+                    }}
+                  >
+                    {intl.formatMessage(messages.remove)}
+                  </Button>
+                </div>
+              }
+            </Col>
+          </Row>
           <p>{intl.formatMessage(messages.description)}</p>
+
           {donationOrgs.length ? (
             <Row className={s.items} gutter={20} type='flex' align='center'>
               {donationOrgs.map((item, i) =>
@@ -89,17 +109,17 @@ class Donation extends React.Component {
                     item={item}
                     imagesProp={'logo'}
                     onClick={() => {
-                      if(donationOrg && donationOrg.id === item.id)
-                      {
-                        this.setState({donationOrg:null})
+                      if (donationOrg && donationOrg.id === item.id) {
+                        this.setState({ donationOrg: null })
                       }
-                      else this.setState({donationOrg:item})}
+                      else this.setState({ donationOrg: item })
+                    }
                     }
                     bordered={false}
                     description={item.description}
                     active={donationOrg && donationOrg.id === item.id}
                     keyValue={ALPHABET[i]}
-                    imageStyle = {{backgroundSize:'contain'}}
+                    imageStyle={{ backgroundSize: 'contain' }}
                   />
                 </Col>
               )}
@@ -113,73 +133,48 @@ class Donation extends React.Component {
               current={doPage}
               total={doCount}
               pageSize={doPageSize}
-              onChange={(current) => getDonationOrgs({pagination: {current}})}
-              itemRender={(current, type, el) => <PaginationItem type={type} el={el}/>}
+              onChange={(current) => getDonationOrgs({ pagination: { current } })}
+              itemRender={(current, type, el) => <PaginationItem type={type} el={el} />}
             />
           </div>
           <Form>
-            <Row>
-              <Col xs={18}>
-                <Form.Item className={s.amount}>
-                  {getFieldDecorator('donationAmount', {
-                    initialValue: donationAmount,
-                    rules: [
-                      { required: true, message: 'Sorry, you must select a donation amount.' },
-                    ],
-                  })(
-                    <Input type='number' placeholder={intl.formatMessage(messages.amount)} />
-                  )}
-                </Form.Item>
-                <Form.Item>
-                  {getFieldDecorator('hideAmount', {
-                    initialValue: hideAmount,
-                    valuePropName: 'checked',
-                  })(
-                    <Checkbox>{intl.formatMessage(messages.hideamount)}</Checkbox>
-                  )}
-                </Form.Item>
-              </Col>
-              <Col xs={6}>
-                {
-                  donationOrg && 
-                  <div className={s.removeBtn}>
-                    <Button
-                      type='primary'
-                      size={'small'}
-                      onClick={()=>{
-                        this.props.setDonationOrg(null);
-                        localStorage.removeItem(DONATION_STATE);
-                        this.props.removeDontationFromBundle()
-                        this.props.submitDonation(null,true)
-                      }}
-                    >
-                      {intl.formatMessage(messages.remove)}
-                    </Button>
-                  </div>
-                }
-              </Col>
-            </Row>
-            
-           
+            <Form.Item className={s.amount}>
+              {getFieldDecorator('donationAmount', {
+                initialValue: donationAmount,
+                rules: [
+                  { required: true, message: 'Sorry, you must select a donation amount.' },
+                ],
+              })(
+                <Input type='number' placeholder={intl.formatMessage(messages.amount)} />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('hideAmount', {
+                initialValue: hideAmount,
+                valuePropName: 'checked',
+              })(
+                <Checkbox>{intl.formatMessage(messages.hideamount)}</Checkbox>
+              )}
+            </Form.Item>
           </Form>
         </div>
         <PurchaseActions>
           <Button
             type='primary'
             disabled={!donationOrg}
-            onClick={(e)=>this.handleSubmit(e,true)}
+            onClick={(e) => this.handleSubmit(e, true)}
           >
             {'SAVE AND BUY MORE PRODUCTS'}
           </Button>
           <KeyHandler
             keyEventName={KEYPRESS}
             keyCode={13}
-            onKeyHandle={(e)=>this.handleSubmit(e)}
+            onKeyHandle={(e) => this.handleSubmit(e)}
           />
           <Button
             type='primary'
             disabled={!donationOrg}
-            onClick={(e)=>this.handleSubmit(e)}
+            onClick={(e) => this.handleSubmit(e)}
           >
             {intl.formatMessage(messages.submit)}
           </Button>
