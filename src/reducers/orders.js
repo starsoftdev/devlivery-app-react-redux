63685e6ce8a8e6663276ff978a28e62477bc30d4
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-12-21 10:17:55 +0200

    After viewing order details, list of orders is always shown on page 1

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -101,7 +101,7 @@ export const openOrderDetailsModal = (order) => (dispatch, getState) => {
 
 export const closeOrderDetailsModal = () => (dispatch, getState, {fetch,history}) => {
   dispatch({type: CLOSE_ORDER_DETAILS_MODAL})
-  history.push('/dashboard/orders');
+  //history.push('/dashboard/orders');
 }
 
 export const getOrderDetails = (order) => (dispatch, getState, {fetch}) => {
