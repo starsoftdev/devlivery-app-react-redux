848547944fad724b2914c4c678175ae26eaf2dba
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-12-27 23:18:38 +0200

    set release version(byzumi@0.0.8)

--- src/client.js
+++ src/client.js
@@ -24,7 +24,7 @@ import Raven from 'raven-js';
 
 !__DEV__ && Raven
     .config('https://0905932c6d084f0482d4ea11b0bc40b8@sentry.io/1272883',{
-      release: '3.1.21'
+      release: 'byzumi@0.0.8'
     })
     .install();
 /* @intl-code-template addLocaleData(${lang}) */
